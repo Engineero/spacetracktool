198c19c6b5476ef3edd01dd0a035722457b3a2a9
Engineero <engineerolabs@gmail.com>
2018-05-01 19:27:32 -0600

    Bumped version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 NAME = 'spacetracktool'
-VERSION = '0.1.0b3'
+VERSION = '0.1.0b4'
 DESCRIPTION = 'A Python API for querying space-track.org'
 LONG_DESCRIPTION = read('README.rst')
 LONG_DESCRIPTION_CONTENT_TYPE = 'text/x-rst'
